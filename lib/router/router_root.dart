13c2182135882c9a653387ee3779205a83aebaf2
TaoEngine <taoengine@qq.com>
2025-12-30 11:09:18 +0800

    更新主页面逻辑

--- lib/router/router_root.dart
+++ lib/router/router_root.dart
@@ -1,5 +1,13 @@
 import 'package:shelf/shelf.dart';
 
 Response rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+  return Response(
+    501,
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8',
+      'Server': 'Outdoor Aerial Server',
+    },
+    body:
+        '<p>抱歉，这个服务器并未安装 Outdoor Aerial 的任意一个客户端，所以会显示这篇说明。</p><p>试着参照这个文档解决此问题：<br/>https://github.com/TaoEngine/outdoor_aerial_server/wiki/935</p>',
+  );
 }
